203313ffe1eddb6f1fa50da01e59ddfb94b17de0
wangye <im.euii@gmail.com>
2017-08-01 23:59:50 +0800

    更新gulp file文件

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,52 @@ var gulp = require('gulp'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
     del = require('del');
+    htmlmin = require('gulp-htmlmin')
 
 gulp.task('styles', function () {
     return gulp.src('src/styles/*.css')
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(rename({ suffix: '.min' }))
         .pipe(minifycss())
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(notify({ message: 'Styles task complete' }));
 });
 
 gulp.task('images', function() {
   return gulp.src('src/images/*')
     .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-    .pipe(gulp.dest('img'))
+    .pipe(gulp.dest('dist/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('imagesPizza', function() {
+  return gulp.src('views/images/*')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('views/images'))
     .pipe(notify({ message: 'Images task complete' }));
 });
 
 gulp.task('clean', function(cb) {
-    del(['img', 'css'], cb)
+    del(['dist/img', 'dist/css'], cb)
 });
 
 gulp.task('scripts', function() {
   return gulp.src('src/scripts/*.js')
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-    .pipe(gulp.dest('js'))
+    .pipe(gulp.dest('dist/js'))
     .pipe(notify({ message: 'Scripts task complete' }));
+});
+gulp.task('minifyHtml', function() {
+  return gulp.src('src/index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'))
+    .pipe(notify({message:'html task complete'}));
 });
 
 // gulp.task('default', ['clean'], function() {
 //     gulp.start('styles', 'images');
 // });
-gulp.task('default', ['styles','images','scripts']);
+gulp.task('default', ['styles','images','scripts','imagesPizza','minifyHtml']);
 
